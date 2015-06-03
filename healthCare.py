b7acc80e9d02a2bb67dbb127efc155d816606868
Taylor Somma <taylorsomma@Taylors-Air.frontier.com>
2015-06-03 17:13:00 -0400

    Add healthCare.py to be tracked

---
+++ healthCare.py
@@ -0,0 +1,21 @@
+import urllib2
+import urllib
+import json
+import pprint
+import pandas
+
+#Use the json module to dump a dictionary to a string for posting
+data_string = urllib.quote(json.dumps({'id':'data-explorer'}))
+
+# Make the HTTP request.
+response = urllib2.urlopen('http://demo.ckan.org/api/3/action/group_list',
+	data_string)
+assert response.code == 200
+
+# Use the json module to Load CKAN's response into a dictionary.
+response_dict = json.loads(response.read())
+
+# Check the contents of the response
+assert response_dict['success'] is True
+result = response_dict['result']
+pprint.pprint(result)
